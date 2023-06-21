1679c32796c62ae5a85d831ea172b21a7f6b4d42
wbail <guilhermedanbail@gmail.com>
2023-06-21 20:13:35 +0000

    feat: fibonachi series

---
+++ fibonachi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/*
+
+Time complexity of the function printNTermsOfFibonachiSeries(int n) is O(n)
+
+*/
+
+int printNTermsOfFibonachiSeries(int n)
+{
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return printNTermsOfFibonachiSeries(n - 2) + printNTermsOfFibonachiSeries(n - 1);
+}
+
+int main ()
+{
+    int i = 0;
+    int n = 10;
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d\n", printNTermsOfFibonachiSeries(i));
+    }
+
+    return 0;
+}
